ffe03652962dd8f1a7c661ce79f850bb9451070b
gxgeek <qw2137846@163.com>
2016-12-31 17:45:11 +0800

    1

--- src/main/java/com/gx/core/controller/CenterController.java
+++ src/main/java/com/gx/core/controller/CenterController.java
@@ -1,8 +1,35 @@
 package com.gx.core.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.support.WebBindingInitializer;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.xml.crypto.Data;
+
 /**
  * Created by gx on 2016/12/27.
  */
 
-public class CenterController {
+@Controller
+public class CenterController  {
+    private Logger  log = Logger.getLogger(CenterController.class);
+    @RequestMapping(value = "",method = RequestMethod.POST)
+    public String test(String name, Data birthday){
+
+        System.out.println("");
+        return null;
+    }
+
+
+
+    //局部绑定
+    @InitBinder
+    public void initBinder(WebDataBinder binder, WebRequest request) {
+
+    }
 }
